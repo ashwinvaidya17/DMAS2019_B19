64cbfc3ab83fff58924e5324bdb36f1e50ffaded
JindeShubhamA <55285264+JindeShubhamA@users.noreply.github.com>
2019-10-13 18:45:37 +0200

    Update run.sh

--- Blotto_game/run.sh
+++ Blotto_game/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python main.py --troops 8 --battlefields 3 --orderofagent1 2 --orderofagent2 1  --simulation 1 &
+python 2playerssimulation.py --troops 8 --battlefields 3 --orderofagent1 2 --orderofagent2 1  --simulation 1 &
 ./blotto_game.x86_64
