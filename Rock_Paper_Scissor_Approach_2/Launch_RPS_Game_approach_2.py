21a4b08cb9376d4eb43a2c0482f03c43209111a6
Dhawal Salvi <salvidhawal@gmail.com>
2019-12-17 00:44:09 +0100

    Update Launch_RPS_Game_approach_2.py

--- Rock_Paper_Scissor_Approach_2/Launch_RPS_Game_approach_2.py
+++ Rock_Paper_Scissor_Approach_2/Launch_RPS_Game_approach_2.py
@@ -8,7 +8,7 @@ player_1_order = int(input("Now, choose the order of thinking for Player 1 (less
 player_2_order = int(input("Similarly, choose the order of thinking for Player 2 (less than 6): "))
 max_rounds = 100
 
-
+# if user chooses order >= 6, player order will become 5 (order limit - 1)
 if player_1_order >= order_limit:
     player_1_order = order_limit-1
 
@@ -22,5 +22,7 @@ for i in range(3, 0, -1):
     print(i)
     time.sleep(1.0)
 print("-----------------------------------------")
-# This function will call rock_paper_scissor function from Rock_Paper_Scissor_Game module
+
+# This function will call rock_paper_scissor function from Rock_Paper_Scissor_Game module where the actual game happens
+# the result is displayed
 rps.rock_paper_scissor(player_1_order, player_2_order, max_rounds)
