8d7a4f74095050ac30ed4a71a90d7fcb142c0658
Dhawal Salvi <salvidhawal@gmail.com>
2019-12-16 20:08:32 +0100

    Update Launch_RPS_Game_approach_2.py

--- Rock_Paper_Scissor_Approach_2/Launch_RPS_Game_approach_2.py
+++ Rock_Paper_Scissor_Approach_2/Launch_RPS_Game_approach_2.py
@@ -2,20 +2,25 @@ import time
 import Rock_Paper_Scissor_Game_and_Rules as rps
 
 print("Rock-Paper-Scissor Game")
-
+order_limit = 6
 # The terminal will ask you to choose the order for player 1 & player 2 (For now choose 0 or 1)
-player_1_order = int(input("Now, choose the order of thinking for Player 1: "))
-player_2_order = int(input("Similarly, choose the order of thinking for Player 2: "))
-end_of_games = int(input("Choose total number of games (eg: 5): "))
-end_of_rounds = int(input("Choose number of rounds in a game (eg: 15): "))
+player_1_order = int(input("Now, choose the order of thinking for Player 1 (less than 6): "))
+player_2_order = int(input("Similarly, choose the order of thinking for Player 2 (less than 6): "))
+max_rounds = 100
 
-# The Game will begins after 2 Seconds
+
+if player_1_order >= order_limit:
+    player_1_order = order_limit-1
+
+if player_2_order >= order_limit:
+    player_2_order = order_limit-1
+
+# The Game will begins after 3 Seconds
 print("\nGame begins in:")
 time.sleep(0.5)
 for i in range(3, 0, -1):
     print(i)
-    time.sleep(0.5)
+    time.sleep(1.0)
 print("-----------------------------------------")
-
-# This will call rock_paper_scissor function from Rock_Paper_Scissor_Game module
-rps.rock_paper_scissor(player_1_order, player_2_order, end_of_games, end_of_rounds)
+# This function will call rock_paper_scissor function from Rock_Paper_Scissor_Game module
+rps.rock_paper_scissor(player_1_order, player_2_order, max_rounds)
